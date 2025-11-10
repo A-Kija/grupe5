5548cd29c7b3adf8900c570dcd092dc2852361ec
A-Kija <42836152+A-Kija@users.noreply.github.com>
2025-11-10 12:16:31 +0200

    011A

---
+++ 011/app.js
@@ -0,0 +1,27 @@
+console.log('Labas, Bebriau!');
+
+
+var x = 10;
+
+console.log(x);
+
+var pasisveikinimasSuBarsuku = 'Labas rytas, ponas Barsukai!';
+
+console.log('pasisveikinimasSuBarsuku', pasisveikinimasSuBarsuku);
+
+x = 20;
+
+console.log(x, pasisveikinimasSuBarsuku);
+
+let L = 20;
+const N = 20;
+
+// 1. Kintamojo vardas
+// 2. Kintamojo reikšmė (turinys)
+// 3. Kintamojo tipas
+
+let K1 = 25; // tipas Number
+let K2 = 'Barsukas' // tipas String
+let K3 = '102' // tipas String
+let K4 = 54.78 // tipas Number
+let K8 = -78.0002 // tipas Number
