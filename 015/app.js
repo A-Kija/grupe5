7ce63eb01c1a9dfae169e6778dcbf4d33ced4a46
A-Kija <42836152+A-Kija@users.noreply.github.com>
2025-11-17 13:30:19 +0200

    015

--- 015/app.js
+++ 015/app.js
@@ -60,3 +60,59 @@ if (52 > 6) {
     fun6();
 }
 
+const fun7 = function(pirmasParametras, antrasParametras) {
+    console.log(pirmasParametras * antrasParametras);
+}
+
+
+let pirmasArgumentas = 2;
+let antrasArgumentas = 5;
+
+fun7(pirmasArgumentas, antrasArgumentas);
+
+fun7(8, 10);
+
+const nr2 = document.querySelector('#nr2');
+const nr3 = document.querySelector('#nr3');
+
+const fun8 = function(sk1, sk2) {
+    const rez = sk1 * sk2;
+    return rez;
+}
+
+nr2.innerText = fun8(8, 4);
+
+const rez1 = fun8(7, 2);
+nr3.innerText = rez1;
+
+// Parašyti funkciją, kuri gauna du skaičius ir grąžina didesnį. Gražintą skaičių atspausdiname konsolėje
+
+
+const fun9 = function(sk1, sk2) {
+    if (sk1 > sk2) {
+        return sk1; // nutraukiam funkcijos veikimą.
+    }
+    return sk2;
+}
+
+// dar ne dabar
+const fancyFun = (sk1, sk2) => sk1 > sk2 ? sk1 : sk2; // arrow function + ternary
+
+console.log(fancyFun(11, 22));
+console.log(fancyFun(11, 2));
+
+nr2.innerText = fun9(11, 2);
+
+
+// Parašyti funkciją, kuri gauna du stringus ir grąžina ilgesnį. Gražintą stringą atspausdiname konsolėje
+
+
+const fun10 = function(st1, st2) {
+    if (st1.length > st2.length) {
+        return st1; // nutraukiam funkcijos veikimą.
+    }
+    return st2;
+}
+
+console.log(fun10('Bebras', 'Barsukas'));
+
