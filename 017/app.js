a0779d359fe0c0273fc4ec5679be983dd8ff71a3
A-Kija <42836152+A-Kija@users.noreply.github.com>
2025-11-19 13:32:23 +0200

    017

--- 017/app.js
+++ 017/app.js
@@ -7,7 +7,7 @@ console.log('Hello, darkness my old friend');
 const buttonNo1 = document.querySelector('#nr1');
 
 // inline funkcija
-buttonNo1.addEventListener('click', function() {
+buttonNo1.addEventListener('click', function () {
     console.log('Button No1');
 });
 
@@ -36,7 +36,7 @@ const buttonNo2 = document.querySelector('#nr2');
 buttonNo2.addEventListener('click', _ => console.log('Button No2'));
 
 
-const sum1 = function(a, b) {
+const sum1 = function (a, b) {
     return a + b;
 }
 
@@ -52,7 +52,7 @@ console.log(sum1(5, 3));
 console.log(sum2(5, 3));
 console.log(sum3(5, 3));
 
-const add5 = function(a) {
+const add5 = function (a) {
     return a + 5;
 }
 
@@ -86,3 +86,66 @@ console.log(bebras1());
 console.log(bebras2());
 console.log(bebras3());
 console.log(bebras4());
+
+console.clear();
+
+for (let i = 0; i < 10; i++) {
+    console.log('Bla bla', i);
+}
+
+// for (let i = 0; i > -10; i--) {
+//     console.log('Bla bla', i);
+// }
+
+// Panaudojimas 1.Radimas 2.Agregacija
+
+const word = 'rabarbaras';
+
+let found = '"r" raidė nerasta';
+
+
+for (let i = 0; i < word.length; i++) {
+    if (word[i] == 'r') {
+        found = `"r" rasta ${i} vietoje`; // užrašas ${i} NIEKO BENDRO su ciklu neturi
+    }
+} // blokas kartojasis
+
+console.log(found); // nėra ciklo bloke ir nesikartoja
+
+let count = 0;
+
+for (let i = 0; i < word.length; i++) {
+    if (word[i] == 'r') {
+        count++; // radome vieną, tai didiname vienetu
+    }
+} // blokas kartojasis
+
+console.log(`Žodyje ${word} yra ${count} "r"`); // nėra ciklo bloke ir nesikartoja
+
+
+
+let found1 = '';
+
+for (let i = 0; i < word.length; i++) {
+    if (word[i] == 'r') {
+        found1 += `"r" rasta ${i} vietoje \n`; // \n spec simbolis "nauja eilutė"
+    }
+} // blokas kartojasis
+
+console.log(found1); // nėra ciklo bloke ir nesikartoja
+
+/*
+
+CIKLAI:
+for
+while
+do while
+for in
+for of
+switch
+
+CIKLO VALDYMAS:
+break
+continue
+
+*/
