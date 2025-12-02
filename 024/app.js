c07c8cdb4ee5e6d0971ba3e1aed99180df019962
A-Kija <42836152+A-Kija@users.noreply.github.com>
2025-12-02 13:30:52 +0200

    024

--- 024/app.js
+++ 024/app.js
@@ -111,7 +111,7 @@ animals.forEach(animal => {
 // 2. Sukuriam patobulintą masyvą iš seno masyvo ===> map
 
 const animalsPlus1Age = animals.map(animal => {
-    return {...animal, age: animal.age + 1} //...animal kopija seno objekto, age: užrašom ant viršaus naują age
+    return { ...animal, age: animal.age + 1 } //...animal kopija seno objekto, age: užrašom ant viršaus naują age
 });
 
 console.log(animalsPlus1Age);
@@ -138,8 +138,128 @@ const allAges = animals.reduce((sum, animal) => {
 console.log(allAges);
 
 
+// 5. Naujo išfiltruoto masyvo sudarymas ===> filter
+
+const youngAnimals = animals.filter(animal => {
+    if (animal.age <= 10) {
+        return true; // tinka pridedam prie išfiltruoto masyvo
+    }
+    return false; // netinka, nededam
+});
+
+console.log(youngAnimals);
 
 
+// 6. Naujo surūšiuoto masyvo sudarymas ===> toSorted
+
+const byAge = animals.toSorted((a, b) => {
+    return b.age - a.age; // jeigu a > b grąžins teigiamą skaičių jeigu a < b grąžins neigiamą
+});
+
+const byAge2 = animals.toSorted((a, b) => {
+    if (a.age > b.age) {
+        return -1;
+    }
+    if (a.age < b.age) {
+        return 1;
+    }
+    return 0;
+})
+
+console.log(byAge, byAge2);
+
+animals.push(
+    {
+        name: 'Žąsis',
+        age: 2
+    },
+    {
+        name: 'Zebras',
+        age: 12
+    },
+    {
+        name: 'Šarka',
+        age: 6
+    },
+    {
+        name: 'Silkė',
+        age: 12
+    }
+);
+
+
+const byName = animals.toSorted((a, b) => {
+    return a.name.localeCompare(b.name, 'lt');
+});
+
+const byName2 = animals.toSorted((a, b) => {
+    if (a.name > b.name) {
+        return 1;
+    }
+    if (a.name < b.name) {
+        return -1;
+    }
+    return 0;
+});
+
+// susortins patį animals masyvą
+animals.sort((a, b) => {
+    return a.name.localeCompare(b.name, 'lt');
+});
+
+console.log(byName, byName2);
+
+console.clear();
+
+const pack = 'L' // S, M, L, XL
+
+
+if (pack == 'S') {
+    console.log('S');
+}
+if (pack == 'S' || pack == 'M') {
+    console.log('M');
+}
+if (pack == 'S' || pack == 'M' || pack == 'L') {
+    console.log('L');
+}
+console.log('XL');
+
+console.log('-----------------');
+
+switch (pack) {
+    case 'S': console.log('S');
+    case 'M': console.log('M');
+    case 'L': console.log('L');
+    default: console.log('XL');
+}
+
+
+if (pack == 'S') {
+    console.log('Mažas');
+} else if (pack == 'M') {
+    console.log('Vidutinis');
+} else if (pack == 'L') {
+    console.log('Didelis');
+} else if (pack == 'XL') {
+    console.log('Labai didelis');
+}
+
+console.log('-----------------');
+
+switch (pack) {
+    case 'S':
+        console.log('Mažas');
+        break;
+    case 'M':
+        console.log('Vidutinis');
+        break;
+    case 'L':
+        console.log('Didelis');
+        break;
+    case 'XL':
+        console.log('Labai didelis');
+}
 
 
 
