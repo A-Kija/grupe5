9484d93774e8a289a4fb1fad30ce126f700663fa
A-Kija <42836152+A-Kija@users.noreply.github.com>
2025-12-18 12:15:48 +0200

    034KAVA

--- 033/src/app.js
+++ 033/src/app.js
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from 'uuid';
+
 
 console.log('CRUD');
 
@@ -92,60 +92,6 @@ const render = _ => {
     });
 }
 
-//* CRUD CODE **//
 
-const readLocalStorage = _ => {
-    let data = localStorage.getItem(KEY);
-    if (null === data) {
-        LIST = [];
-    } else {
-        LIST = JSON.parse(data);
-    }
-}
-
-const writeLocalStorage = _ => {
-    let data = JSON.stringify(LIST);
-    localStorage.setItem(KEY, data);
-}
-
-
-/*
-Store vykdo naujo "daikto" įrašymą į saugyklą
-Turi gauti "daiktą"
-Turi "daiktui" priskirt ID ir įrašyti į saugyklą
-*/
-const Store = data => {
-    const id = uuidv4();
-    const dataToStore = {
-        ...data,
-        id
-    }
-    LIST.unshift(dataToStore);
-    writeLocalStorage();
-    render();
-}
-
-/*
-Destroy vykdo "daikto" pašalinimą iš saugyklos
-Turi gauti "daikto" identifikatorių
-Turi pašalinti daiktą su nurodytu identifikatorium
-*/
-const Destroy = id => {
-    LIST = LIST.filter(item => item.id != id);
-    writeLocalStorage();
-    render();
-}
-
-/*
-Update vykdo redaguoto "daikto" saugojimą saugykloje
-Turi gauti "daikto" identifikatorių ir daikto naujas savybes
-Turi persaugoti daiktą su nurodytu identifikatorium ir naujom savybėm
-*/
-
-const Update = (id, data) => {
-    LIST = LIST.map(item => item.id == id ? {...item, ...data, id} : item);
-    writeLocalStorage();
-    render();
-};
 
 init();
--- obj/app.js
+++ obj/app.js
@@ -1,24 +1,33 @@
 console.log('OOP');
 
+// Rašo Marytė
 
 class Bebras {
     // instrukcija objekto gamybai
 
     // metodas (funkcija) kuri AUTOMATIŠKAI pasileidžia objekto kūrimo (gaminimo) metu
 
-    constructor() {
+    constructor(metai) {
         console.log('KONSTRUOJAM!');
 
-        this.bebroMetai = 12;
+        this.bebroMetai = metai;  // this === b1, kitą kartą this === b2, , dar kitą kartą this === b3
+
+        // this nuoroda į dar neegzistuojantį objektą "tas"
+    }
+
+
+    kiekMetu() {
+        console.log('Bebrui yra: ' + this.bebroMetai + ' metų');
     }
 
 
 
 }
 
+//Rašo Petriukas
 
 // Gaminame objektą pagal instrukciją
-const b1 = new Bebras();
+const b1 = new Bebras(10);
 
 // b1 yra objektas pagamintas pagas Bebras klasės instrukcijas
 
@@ -26,9 +35,40 @@ console.log(b1);
 
 // galim tokių prisigamint daug
 
-const b2 = new Bebras();
-const b3 = new Bebras();
+const b2 = new Bebras(12);
+const b3 = new Bebras(16);
 
 b3.bebroMetai = 14;
 
 console.log(b2, b3);
+
+b2.kiekMetu();
+
+
+
+// Sukurti 3 namų objektus iš klasės Namas. Kiekvienas namas turi turėti
+// skirtingą kambarių ir kaminų kiekį
+
+class Namas {
+
+    constructor(kambariai, kaminai) {
+        this.kambariai = kambariai; // kambariai kintamasis; this.kambariai objekto savybė
+        this.kaminai = kaminai;
+    }
+
+    kamIrKam() {
+        const sandauga = this.kambariai * this.kaminai;
+        console.log('Sandauga: ' + sandauga);
+    }
+}
+
+const n1 = new Namas(4, 5);
+const n2 = new Namas(7, 1);
+const n3 = new Namas(2, 0);
+
+console.log(n1, n2, n3);
+
+n2.kamIrKam();
+
+
+// Parašyti metodą kuris atspausdina kaminų ir kambarių sandaugą
