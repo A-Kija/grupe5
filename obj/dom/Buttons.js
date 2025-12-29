e8fd3ae732c54d8cf0259622eb5d15aa9e9c7e07
A-Kija <42836152+A-Kija@users.noreply.github.com>
2025-12-29 12:16:13 +0200

    037KAVA

---
+++ obj/dom/Buttons.js
@@ -0,0 +1,13 @@
+
+class Buttons {
+
+    constructor() {
+        this.buttons = document.querySelectorAll('button');
+    }
+
+
+}
+
+new Buttons(); 
+// Paleidžiame klasę Buttons. 
+// Objekto niekam nepriskiriame, nes mums nereikia prie jo vėliau kreiptis.
