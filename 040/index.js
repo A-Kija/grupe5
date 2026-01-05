e942ac43e68a756a45578142691c11714c032163
A-Kija <42836152+A-Kija@users.noreply.github.com>
2026-01-05 13:32:59 +0200

    040

--- 040/index.js
+++ 040/index.js
@@ -6,15 +6,63 @@ const port = 80;
 // Failai folderyje 'public' bus pasiekiami per naršyklę
 app.use(express.static('public')); // Nurodome, kad statiniai failai bus iš 'public' katalogo
 
+
 // Dalis Router
 app.get('/', (req, res) => {
 
-    // read file synchronously
+    // skaitome tris atskirus failus. Toks skaitymas galimas tik backend'e
     const top = fs.readFileSync('./html/top.html', 'utf8');
     const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
     const home = fs.readFileSync('./html/home.html', 'utf8');
 
-    res.send(top + home + bottom);
+    const pageTitle = 'Home Page';
+    const topWithTitle = top.replace('{{title}}', pageTitle);
+
+    res.send(topWithTitle + home + bottom); // jau vienas HTML failas
+
+});
+
+app.get('/services', (req, res) => {
+
+    // skaitome tris atskirus failus. Toks skaitymas galimas tik backend'e
+    const top = fs.readFileSync('./html/top.html', 'utf8');
+    const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
+    const services = fs.readFileSync('./html/services.html', 'utf8');
+
+    const pageTitle = 'Services Page';
+    const topWithTitle = top.replace('{{title}}', pageTitle);
+
+    res.send(topWithTitle + services + bottom); // jau vienas HTML failas
+
+});
+
+
+app.get('/about', (req, res) => {
+
+    // skaitome tris atskirus failus. Toks skaitymas galimas tik backend'e
+    const top = fs.readFileSync('./html/top.html', 'utf8');
+    const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
+    const about = fs.readFileSync('./html/about.html', 'utf8');
+
+    const pageTitle = 'About Page';
+    const topWithTitle = top.replace('{{title}}', pageTitle);
+
+    res.send(topWithTitle + about + bottom); // jau vienas HTML failas
+
+});
+
+
+app.get('/contact', (req, res) => {
+
+    // skaitome tris atskirus failus. Toks skaitymas galimas tik backend'e
+    const top = fs.readFileSync('./html/top.html', 'utf8');
+    const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
+    const contact = fs.readFileSync('./html/contact.html', 'utf8');
+
+    const pageTitle = 'Contact Page';
+    const topWithTitle = top.replace('{{title}}', pageTitle);
+
+    res.send(topWithTitle + contact + bottom); // jau vienas HTML failas
 
 });
 
