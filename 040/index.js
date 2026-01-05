641ce754c004c0b65c344598d2f62ecb815c79c4
A-Kija <42836152+A-Kija@users.noreply.github.com>
2026-01-05 12:16:06 +0200

    040KAVA

---
+++ 040/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const fs = require('fs'); // failų sistemos modulis-biblioteka
+const app = express();
+const port = 80;
+
+// Failai folderyje 'public' bus pasiekiami per naršyklę
+app.use(express.static('public')); // Nurodome, kad statiniai failai bus iš 'public' katalogo
+
+// Dalis Router
+app.get('/', (req, res) => {
+
+    // read file synchronously
+    const top = fs.readFileSync('./html/top.html', 'utf8');
+    const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
+    const home = fs.readFileSync('./html/home.html', 'utf8');
+
+    res.send(top + home + bottom);
+
+});
+
+
+// Paleidžia serverį ir parašo terminale, kad viskas yra gerai.
+app.listen(port, () => {
+    console.log(`Viskas gerai. Bebras dirba ant ${port} porto`);
+});
