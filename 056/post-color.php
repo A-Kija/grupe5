6f4e6330db3979370442ace41929d649554301e2
A-Kija <42836152+A-Kija@users.noreply.github.com>
2026-02-02 13:35:02 +0200

    056

--- 056/post-color.php
+++ 056/post-color.php
@@ -1,3 +1,32 @@
+<?php
+/*
+BE JS (tradicinis) siunčiam pilną HTML puslapį
+SU JS siunčiam tik mažą puslapio dalį
+
+Tradicinis:
+Jeigu metodas GET ---> serveris grąžina HTML
+Jeigu metodas POST ---> serveris grąžina redirect (duodam linką, kuriame yra HTML)
+
+Su JS šitas NEGALIOJA (galima bet nebūtina po POST grąžinti redirect)
+
+echo ---> išvedimas į response body
+
+
+*/
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['color'])) {
+
+    $color = htmlspecialchars($_POST['color']);
+
+    // setcookie('selected_color', $color, time() + (86400 * 30), '/'); // Cookie valid for 30 days
+
+    $_SESSION['my_color'] = $color;
+
+    header('Location: post-color.php'); // redirektas.
+    exit; // tam kad redirektintų, turim užbaigti kodo vykdymą
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,17 +44,33 @@
     </form>
 
     <?php
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['color'])) {
+        // if (isset($_COOKIE['selected_color'])) {
+        //     $color = htmlspecialchars($_COOKIE['selected_color']);
 
-        header('Location: post-color.php'); // redirektas.
-        exit; // tam kad redirektintų, turim užbaigti kodo vykdymą
+        //     setcookie('selected_color', '', time() - 3600, '/'); // Delete cookie
+            
+        //     echo '<div style="margin-top: 20px;">';
+        //     echo "<p>Selected color: <strong>$color</strong></p>";
+        //     echo "<div style=\"width: 100px; height: 100px; background-color: $color; border: 1px solid #000;\"></div>";
+        //     echo '</div>';
 
-        $color = htmlspecialchars($_POST['color']);
-        echo '<div style="margin-top: 20px;">';
-        echo "<p>Selected color: <strong>$color</strong></p>";
-        echo "<div style=\"width: 100px; height: 100px; background-color: $color; border: 1px solid #000;\"></div>";
-        echo '</div>';
-    }
+
+        // }
+
+        if (isset($_SESSION['my_color'])) {
+            $color = htmlspecialchars($_SESSION['my_color']);
+
+            unset($_SESSION['my_color']);
+            
+            echo '<div style="margin-top: 20px;">';
+            echo "<p>Selected color: <strong>$color</strong></p>";
+            echo "<div style=\"width: 100px; height: 100px; background-color: $color; border: 1px solid #000;\"></div>";
+            echo '</div>';
+
+
+        }
     ?>
+
+
 </body>
 </html>
