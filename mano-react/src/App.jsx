4b149b6978d693f4d2e8bd69790be85ee0a7e49f
A-Kija <42836152+A-Kija@users.noreply.github.com>
2026-03-05 13:36:37 +0200

    074

--- mano-react/src/App.jsx
+++ mano-react/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css';
 import Bebras from './components/01/Bebras';
+import Zb from './components/01/ZaliasBarsukas';
 
 function App() {
 
   return (
     <div className="App">
       <h1>Mano <br/> React</h1>
-      <Bebras></Bebras>
+      <Bebras koks='geras' dydis='80px'></Bebras>
+      <Bebras koks='piktas' dydis='40px'></Bebras>
+      <Zb></Zb>
     </div>
   );
 }
